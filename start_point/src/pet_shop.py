09c46856658ef4eb2216e44cb5a98578760b09e2
Jan Metelski <janekmetelski@hotmail.com>
2021-01-17 16:52:25 +0000

    Something changed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -12,3 +12,17 @@ def add_or_remove_cash(pet_shop, cash):
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, pets_sold):
+    pet_shop["admin"]["pets_sold"] += pets_sold
+    return pet_shop["admin"]["pets_sold"]
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pet_search = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pet_search.append(pet)
+    return pet_search
